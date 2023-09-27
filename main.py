be371fe3b7d1c0e329027dc7c7eac3c1e5b5da7e
shraddhajadhav111 <75525364+shraddhajadhav111@users.noreply.github.com>
2023-09-27 13:22:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+from flask import Flask, jsonify
 from src.data_fetcher import load_data
 from src.data_preprocessor import select_and_preprocess_data
 from src.model import main as model_main
 
+app = Flask(__name__)
+
+@app.route('/run-pipeline', methods=['GET'])
 def run_pipeline():
     # Load the data
     df = load_data(r"C:\Users\shjadhav\OneDrive - Malomatia\Desktop\ml_cicd\data\train.csv")
@@ -12,7 +16,7 @@ def run_pipeline():
     # Train the model, evaluate it, and log results with mlflow
     results = model_main(X_train, y_train, X_test, y_test)
 
-    print(results)
+    return jsonify(results)
 
 if __name__ == "__main__":
-    run_pipeline()
+    app.run(debug=True)
